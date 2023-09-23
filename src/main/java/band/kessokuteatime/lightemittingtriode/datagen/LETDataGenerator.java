d426aa434f875dc98ec2af1c1ee58c9065fbcf25
KrLite <68179735+KrLite@users.noreply.github.com>
2023-09-23 14:34:17 +0800

    loottables

--- src/main/java/band/kessokuteatime/lightemittingtriode/datagen/LETDataGenerator.java
+++ src/main/java/band/kessokuteatime/lightemittingtriode/datagen/LETDataGenerator.java
@@ -7,10 +7,14 @@ import band.kessokuteatime.lightemittingtriode.content.block.LampBlock;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.block.Block;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.client.*;
+import net.minecraft.data.server.loottable.BlockLootTableGenerator;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.DyeColor;
@@ -23,6 +27,7 @@ import org.apache.logging.log4j.util.TriConsumer;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -38,6 +43,7 @@ public class LETDataGenerator implements DataGeneratorEntrypoint {
 
         pack.addProvider(Language::new);
         pack.addProvider(Model::new);
+        pack.addProvider(BlockLootTable::new);
     }
 
     private static class Language extends FabricLanguageProvider {
@@ -53,17 +59,20 @@ public class LETDataGenerator implements DataGeneratorEntrypoint {
         @Override
         public void generateTranslations(TranslationBuilder translationBuilder) {
             try {
+                // Parse existing English translations
                 translationBuilder.add(dataOutput.getModContainer().findPath("assets/let/lang/en_us.fundamentals.json").orElseThrow());
             } catch (IOException ioException) {
                 throw new RuntimeException(ioException);
             }
 
+            // Add English translations for colors
             Arrays.stream(DyeColor.values()).forEach(dyeColor ->
                     translationBuilder.add(LET.idString("color", dyeColor.getName()), formatDyeColor(dyeColor))
             );
         }
 
         private static String formatDyeColor(DyeColor dyeColor) {
+            // light_blue -> Light Blue
             return Arrays.stream(dyeColor.getName().split("_"))
                     .map(StringUtils::capitalize)
                     .collect(Collectors.joining(" "));
@@ -75,25 +84,49 @@ public class LETDataGenerator implements DataGeneratorEntrypoint {
             super(output);
         }
 
+        // Uploads an item model with an existing parent
         private static final TriConsumer<ItemModelGenerator, Identifier, Item> uploadModelWithParent = (itemModelGenerator, identifier, item) ->
                 new net.minecraft.data.client.Model(Optional.of(identifier), Optional.empty())
                         .upload(ModelIds.getItemModelId(item), TextureMap.layer0(item), itemModelGenerator.writer);
 
         @Override
         public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-            Arrays.stream(LETRegistries.Blocks.Type.values()).forEach(type ->
-                    type.getBlockItemMap().forEach((block, item) -> blockStateModelGenerator.blockStateCollector.accept(
-                            ((LampBlock) block).generateBlockStates(type).apply(blockStateModelGenerator, block)
-                    ))
-            );
+            // Use specified generators for blocks
+            Arrays.stream(LETRegistries.Blocks.Type.values())
+                    .forEach(type -> type.getBlockItemMap()
+                            .forEach((block, item) -> blockStateModelGenerator.blockStateCollector.accept(
+                                    ((LampBlock) block).generateBlockStates(type).apply(blockStateModelGenerator, block)
+                            ))
+                    );
         }
 
         @Override
         public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+            // Use generic block ids for items
             Arrays.stream(LETRegistries.Blocks.Type.values()).forEach(type ->
-                    type.getBlockItemMap().forEach((block, item) -> uploadModelWithParent.accept(
-                            itemModelGenerator, type.getIdPack().blockId(), item
+                    type.getBlockItemMap().forEach((block, blockItem) -> uploadModelWithParent.accept(
+                            itemModelGenerator, type.getIdPack().blockId(), blockItem
                     ))
+            );
+        }
+    }
+
+    private static class BlockLootTable extends FabricBlockLootTableProvider {
+        protected BlockLootTable(FabricDataOutput dataOutput) {
+            super(dataOutput);
+        }
+
+        /**
+         * Implement this method to add block drops.
+         *
+         * <p>Use the range of {@link net.minecraft.data.server.loottable.BlockLootTableGenerator#addDrop} methods
+         * to generate block drops.
+         */
+        @Override
+        public void generate() {
+            // Let blocks drop themselves
+            Arrays.stream(LETRegistries.Blocks.Type.values()).forEach(type ->
+                    type.getBlockItemMap().forEach(this::addDrop)
             );
         }
     }
